7bb4f4a7391a29816ca763d020e4a166fbd1c0a8
Jun <junseokpark.kr@gmail.com>
2023-10-31 10:51:51 -0400

    add intermediate results

---
+++ benchmark/toolTest_pipeline/temp/submit_jobs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sbatch < /n/data1/bch/genetics/lee/projects/SMaHT/results/xTea_mosaic/TITR_1/L1/run_xTEA_pipeline.sh
+sbatch < /n/data1/bch/genetics/lee/projects/SMaHT/results/xTea_mosaic/TITR_1/Alu/run_xTEA_pipeline.sh
