465701c612a113b8a055d71f1cf3651bf0e53b64
Jun <junseokpark.kr@gmail.com>
2023-11-19 21:58:56 -0500

    update xTea multi

--- scripts/xTea_mosaic/run_multiple_samples.sh
+++ scripts/xTea_mosaic/run_multiple_samples.sh
@@ -2,14 +2,14 @@
 
 BASE_DIRECTORY="/n/data1/bch/genetics/lee/projects/SMaHT"
 REF_DIRECTORY=${BASE_DIRECTORY}/refs
-RESULT_DIRECTORY=${BASE_DIRECTORY}/results/xTea/mosaic/200x
+RESULT_DIRECTORY=${BASE_DIRECTORY}/results/xTea/germline/Simul50x
 CONFIG_DIRECTORY=${BASE_DIRECTORY}/benchmark/toolTest_pipeline/config
-
-SAMPLE_DIRECTORY="/n/no_backup2/bch/lee/data/mixedDataRetroSom/A_200x"
+SAMPLE_DIRECTORY="/n/no_backup2/bch/lee/data/mixedDataRetroSom/D_Simul50x"
 SAMPLE_EXT_PREFIX=".recal.sorted.bam"
-SAMPLES=("CONT_2" "CONT_3" "CONT_4" "TITR_1" "TITR_4" "TITR_5" "TITR_6") # "CONT_10" "CONT_11" "CONT_12" "CONT_13" "CONT_14")
+SAMPLES=("CONT_15" "TITR_15") 
 
 X_TEA_BIN=/n/data1/bch/genetics/lee/tools/xTea-mosaic/xtea
+X_TEA_RUN_SCRIPT=xtea
 
 # Loop through the array and process each value
 for SAMPLE_ID in "${SAMPLES[@]}"; do
@@ -19,5 +19,6 @@ for SAMPLE_ID in "${SAMPLES[@]}"; do
         -e ${REF_DIRECTORY} \
         -r ${RESULT_DIRECTORY} \
         -c ${CONFIG_DIRECTORY} \
-        -x ${X_TEA_BIN} 
+        -x ${X_TEA_BIN} \
+        -f ${X_TEA_RUN_SCRIPT} 
 done
