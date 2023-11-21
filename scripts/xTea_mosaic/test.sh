7847c1beb57c5a2715cd279a5c3d48854a3cc984
aleelab <aleelab.root@gmail.com>
2023-11-20 19:26:14 -0500

    update test.sh

--- scripts/xTea_mosaic/test.sh
+++ scripts/xTea_mosaic/test.sh
@@ -11,24 +11,26 @@ BASE_DIRECTORY="/n/data1/bch/genetics/lee/projects/SMaHT"
 #     -x /n/data1/bch/genetics/lee/tools/xTea-mosaic/xtea
 
 # Germline
-# ./run_xTea.sh -s CONT_8 \
-#     -p .recal.sorted.bam \
-#     -d /n/no_backup2/bch/lee/data/mixedDataRetroSom/B_Simul100x \
-#     -e ${BASE_DIRECTORY}/refs \
-#     -r ${BASE_DIRECTORY}/results/xTea/germline/Simul100x \
-#     -c ${BASE_DIRECTORY}/benchmark/toolTest_pipeline/config \
-#     -x /n/data1/bch/genetics/lee/tools/xTea-mosaic/xtea \
-#     -f xtea
+./run_xTea.sh -s HG002 \
+    -p .GRCh38.300x.bam \
+    -d /n/data1/bch/genetics/lee/data/GIAB/HG002_NA24385_son/NIST_HiSeq_HG002_Homogeneity-10953946/NHGRI_Illumina300X_AJtrio_novoalign_bams \
+    -e ${BASE_DIRECTORY}/refs \
+    -r ${BASE_DIRECTORY}/results/xTea/shortread/germline/GIAB/Illumina300x \
+    -c ${BASE_DIRECTORY}/benchmark/toolTest_pipeline/config \
+    -x /n/data1/bch/genetics/lee/tools/xTea/xtea \
+    -f xtea \
+    -o 16 \
+    -m 128
 
 # LongRead
-./run_xTea.sh -s HG002 \
-    -p _GRCh38_ONT-UL_GIAB_20200122.phased \
-    -d /n/data1/bch/genetics/lee/data/GIAB/Ultralong_OxfordNanopore/guppy-V3.2.4_2020-01-22 \
-    -e ${BASE_DIRECTORY}/refs \
-    -r ${BASE_DIRECTORY}/results/xTea/longread_germline \
-    -c ${BASE_DIRECTORY}/benchmark/toolTest_pipeline/config \
-    -x /n/data1/bch/genetics/lee/tools/xTea-longread/xtea_long \
-    -f xtea_long \
-    -o 16 \
-    -m 128 \
+# ./run_xTea.sh -s HG002 \
+#     -p _GRCh38_ONT-UL_GIAB_20200122.phased \
+#     -d /n/data1/bch/genetics/lee/data/GIAB/Ultralong_OxfordNanopore/guppy-V3.2.4_2020-01-22 \
+#     -e ${BASE_DIRECTORY}/refs \
+#     -r ${BASE_DIRECTORY}/results/xTea/longread_germline \
+#     -c ${BASE_DIRECTORY}/benchmark/toolTest_pipeline/config \
+#     -x /n/data1/bch/genetics/lee/tools/xTea-longread/xtea_long \
+#     -f xtea_long \
+#     -o 16 \
+#     -m 128 \
 
