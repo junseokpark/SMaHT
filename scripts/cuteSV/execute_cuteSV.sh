cb5e9ef2c9f512a7b34c19cd9b8d0943ec421eb2
Jun <junseokpark.kr@gmail.com>
2023-11-28 10:33:10 -0500

    cuteSV

--- scripts/cuteSV/execute_cuteSV.sh
+++ scripts/cuteSV/execute_cuteSV.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -c 32
+#SBATCH -c 16
 #SBATCH -t 4-12:00
 #SBATCH --mem=128G
 #SBATCH -p medium
-#SBATCH -J cutesv-HG003
-#SBATCH -o /n/data1/bch/genetics/lee/projects/SMaHT/results/CuteSV/germline/GIAB/HG003/HG003_%j.out
+#SBATCH -J cutesv-HG002
+#SBATCH -o /n/data1/bch/genetics/lee/projects/SMaHT/results/CuteSV/germline/GIAB/HG002/HG002_%j.out
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=junseok.park@childrens.harvard.edu
 
@@ -18,18 +18,18 @@ conda activate cutesv
 BASE_DIRECTORY=/n/data1/bch/genetics/lee
 REF_DIRECTORY=${BASE_DIRECTORY}/reference/hg38
 WORK_DIRECTORY=${BASE_DIRECTORY}/projects/SMaHT/results/CuteSV/germline/GIAB
-RESULT_DIRECTORY=${WORK_DIRECTORY}/HG003
+RESULT_DIRECTORY=${WORK_DIRECTORY}/HG002
 SOURCE_DIRECTORY=${BASE_DIRECTORY}/data/GIAB
 
 REFERENCE_FILE=${REF_DIRECTORY}/Homo_sapiens_assembly38.fasta
-OUTPUT_FILE=${RESULT_DIRECTORY}/HG003-cutesv.vcf
-BAM_FILE=${SOURCE_DIRECTORY}/HG003_NA24149_father/UCSC_Ultralong_OxfordNanopore_Promethion/HG003_GRCh38_ONT-UL_UCSC_20200508.bam
+OUTPUT_FILE=${RESULT_DIRECTORY}/HG002-cutesv.vcf
+BAM_FILE=${SOURCE_DIRECTORY}/HG002_NA24385_son/Ultralong_OxfordNanopore/guppy-V3.2.4_2020-01-22/HG002_GRCh38_ONT-UL_GIAB_20200122.phased.bam
 
-cuteSV --thread 32 \
+cuteSV --thread 16 \
     --max_cluster_bias_INS 100 \
-	--diff_ratio_merging_INS 0.3 \
-	--max_cluster_bias_DEL 100 \
-	--diff_ratio_merging_DEL 0.3 \
+    --diff_ratio_merging_INS 0.3 \
+    --max_cluster_bias_DEL 100 \
+    --diff_ratio_merging_DEL 0.3 \
     $BAM_FILE \
     $REFERENCE_FILE \
     $OUTPUT_FILE \
